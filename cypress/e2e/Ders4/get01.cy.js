923cb5eb9f00505366c24a85898dce724d756bd5
Gulsah <gulsah.tankos@iuc.edu.tr>
2026-05-12 21:43:10 +0300

    Api Test Update

--- cypress/e2e/Ders4/get01.cy.js
+++ cypress/e2e/Ders4/get01.cy.js
@@ -56,6 +56,7 @@ describe("API testing", ()=>{
 
 });
 
+//Post isteği body gönderme::::::::1
     it("TC04_POST isteği body", ()=>{
 
     const rq ={
@@ -80,9 +81,33 @@ describe("API testing", ()=>{
 
 })
 
+//Post isteği body gönderme:::::::2
+     it("TC05_POST isteği body", ()=>{
+
+        const body ={
+            name:"M.",
+            role:"QA",
+            age:30
+        },
+        const rq ={
+        method: 'POST',
+        url:"https://httpbin.org/post",
+        body:body,
+
+        failOnStatusCode:false
+
+        }
+       
+    }
+
+    cy.request(rq).then((response)=>{
+
+        assert.equal(response.status,200);
+        expect(response.status).to.eq(200);
+        cy.log("response body :::::::: ",JSON.stringify(response.body))
 
 
-
+    })
 
 
 })
